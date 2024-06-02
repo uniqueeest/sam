8d846c5006caabf4e9776f5145aa98ba51d298d5
Yoonjae Choi <dbswo9795@gmail.com>
2024-06-03 00:06:21 +0900

    fix: 삭제 버튼 추가

--- vanilla-framwork/src/components/Items.js
+++ vanilla-framwork/src/components/Items.js
@@ -8,16 +8,29 @@ export default class Items extends Component {
     const { items } = this.state;
     return `
       <ul>
-        ${items.map((item) => `<li>${item}</li>`).join('')}
+        ${items
+          .map(
+            (item, key) => `
+          <li>
+            ${item}
+            <button class="deleteBtn" data-index="${key}">삭제</button>
+          </li>
+        `
+          )
+          .join('')}
       </ul>
-      <button>추가</button>
+      <button class="addBtn">추가</button>
     `;
   }
-
   setEvent() {
-    this.$target.querySelector('button').addEventListener('click', () => {
-      const { items } = this.state;
+    this.addEvent('click', '.addBtn', ({ target }) => {
+      const items = [...this.state.items];
       this.setState({ items: [...items, `item${items.length + 1}`] });
+    });
+    this.addEvent('click', '.deleteBtn', ({ target }) => {
+      const items = [...this.state.items];
+      items.splice(target.dataset.index, 1);
+      this.setState({ items });
     });
   }
 }
