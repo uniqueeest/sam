db2a4aae8d9c5e64f8ef692e324c2f5b3e7e1068
Yoonjae Choi <dbswo9795@gmail.com>
2024-06-03 23:07:52 +0900

    feat: 아이템 컴포넌트 분리

--- vanilla-framwork/src/components/Items.js
+++ vanilla-framwork/src/components/Items.js
@@ -1,36 +1,38 @@
 import Component from '../core/Component.js';
 
 export default class Items extends Component {
-  setup() {
-    this.state = { items: ['item1', 'item2'] };
-  }
   template() {
-    const { items } = this.state;
+    const { filteredItems } = this.props;
     return `
       <ul>
-        ${items
+        ${filteredItems
           .map(
-            (item, key) => `
-          <li>
-            ${item}
-            <button class="deleteBtn" data-index="${key}">삭제</button>
+            ({ contents, active, seq }) => `
+          <li data-seq="${seq}">
+            ${contents}
+            <button class="toggleBtn" style="color: ${
+              active ? '#09F' : '#F09'
+            }">
+              ${active ? '활성' : '비활성'}
+            </button>
+            <button class="deleteBtn">삭제</button>
           </li>
         `
           )
           .join('')}
       </ul>
-      <button class="addBtn">추가</button>
     `;
   }
+
   setEvent() {
-    this.addEvent('click', '.addBtn', ({ target }) => {
-      const items = [...this.state.items];
-      this.setState({ items: [...items, `item${items.length + 1}`] });
-    });
+    const { deleteItem, toggleItem } = this.props;
+
     this.addEvent('click', '.deleteBtn', ({ target }) => {
-      const items = [...this.state.items];
-      items.splice(target.dataset.index, 1);
-      this.setState({ items });
+      deleteItem(Number(target.closest('[data-seq').dataset.seq));
+    });
+
+    this.addEvent('click', '.toggleBtn', ({ target }) => {
+      toggleItem(Number(target.closest('[data-seq]').dataset.seq));
     });
   }
 }
