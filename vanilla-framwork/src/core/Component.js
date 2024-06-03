5c01d401da4ed3f550e13a815692763caed29510
Yoonjae Choi <dbswo9795@gmail.com>
2024-06-03 23:08:26 +0900

    fix: 마운트, 프롭스 추가

--- vanilla-framwork/src/core/Component.js
+++ vanilla-framwork/src/core/Component.js
@@ -1,18 +1,22 @@
 export default class Component {
   $target;
+  props;
   state;
-  constructor($target) {
+  constructor($target, props) {
     this.$target = $target;
+    this.props = props;
     this.setup();
     this.setEvent();
     this.render();
   }
   setup() {}
+  mounted() {}
   template() {
     return '';
   }
   render() {
     this.$target.innerHTML = this.template();
+    this.mounted();
   }
   setEvent() {}
   setState(newState) {
@@ -20,7 +24,6 @@ export default class Component {
     this.render();
   }
   addEvent(eventType, selector, callback) {
-    const children = [...this.$target.querySelectorAll(selector)];
     this.$target.addEventListener(eventType, (event) => {
       if (!event.target.closest(selector)) return false;
       callback(event);
