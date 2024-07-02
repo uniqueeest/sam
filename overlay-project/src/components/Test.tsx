0673756d5b32a14c870ca30bdacdaeae1ba3b29e
Yoonjae Choi <dbswo9795@gmail.com>
2024-07-02 23:44:00 +0900

    fix: test 컴포넌트 수정

--- overlay-project/src/components/Test.tsx
+++ overlay-project/src/components/Test.tsx
@@ -1,11 +1,37 @@
+import { useState } from 'react';
+
 import { useToast } from '../contexts/ToastContext';
+import { saveData } from '../apis/dataApi';
 
 export const Test = () => {
   const { showToast } = useToast();
+
+  const [data, setData] = useState('');
 
   const handleClick = () => {
     showToast('성공', '테스트 성공');
   };
 
-  return <button onClick={handleClick}>버튼 클릭</button>;
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const success = await saveData({ data });
+    if (success) {
+      setData('');
+    }
+  };
+
+  return (
+    <article className="flex flex-col gap-6">
+      <button onClick={handleClick}>컴포넌트 내부 토스트</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={data}
+          onChange={(e) => setData(e.target.value)}
+          placeholder="데이터 입력"
+        />
+        <button type="submit">저장</button>
+      </form>
+    </article>
+  );
 };
