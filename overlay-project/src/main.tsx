bce64025e467e5420d698a588ec1e6acf673ff5f
Yoonjae Choi <dbswo9795@gmail.com>
2024-07-02 23:43:48 +0900

    fix: msw 연결

--- overlay-project/src/main.tsx
+++ overlay-project/src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+if (import.meta.env.DEV) {
+  const { worker } = await import('./mocks/server.ts');
+  worker.start();
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
