b0cc8e866b43dffbea22cab86bca378d23e194bd
Yoonjae Choi <dbswo9795@gmail.com>
2024-07-20 15:10:54 +0900

    feat: 퍼널 방식으로 컴포넌트 연결

--- funnel-project/src/App.tsx
+++ funnel-project/src/App.tsx
@@ -1,5 +1,45 @@
+import { useState } from 'react';
+
+import { FirstStep, SecondStep, ThirdStep, FourthStep } from './components';
+
+type Step = 'first' | 'second' | 'third' | 'fourth';
+
 function App() {
-  return <div>hello world</div>;
+  const [data, setData] = useState({});
+  const [step, setStep] = useState<Step>('first');
+
+  const handleNext = (stepData: any, nextStep: Step) => {
+    setData((prev) => ({ ...prev, ...stepData }));
+    setStep(nextStep);
+  };
+
+  const handleSubmit = async (finalData: any) => {
+    const allData = { ...data, ...finalData };
+
+    try {
+      await sendDataToAPI(allData);
+      setStep('fourth');
+    } catch (error) {
+      console.error('API 호출 실패:', error);
+    }
+  };
+
+  return (
+    <div>
+      {step === 'first' && (
+        <FirstStep onNext={(data) => handleNext(data, 'second')} />
+      )}
+      {step === 'second' && (
+        <SecondStep onNext={(data) => handleNext(data, 'third')} />
+      )}
+      {step === 'third' && <ThirdStep onSubmit={handleSubmit} />}
+      {step === 'fourth' && <FourthStep />}
+    </div>
+  );
 }
 
 export default App;
+
+async function sendDataToAPI(data: any) {
+  console.log('Sending data to API:', data);
+}
